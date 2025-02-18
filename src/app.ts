8f93fe71d4e02ba4a121830f9b1a44e3cecfb01c
Paul Wooten <paul.wooten@hyland.com>
2025-02-18 13:55:37 -0500

    feat: express static

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const port = 3000;
 const app = express();
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
-});
+app.use(express.static('public'));
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
