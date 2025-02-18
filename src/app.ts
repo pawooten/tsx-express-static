ebde98ef0c32b47bd7794ab25d397c661fc832a0
Paul Wooten <paul.wooten@hyland.com>
2025-02-18 13:46:37 -0500

    feat: hello world plaintext response

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('Hello World');
+import express from 'express';
+
+const port = 3000;
+const app = express();
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
